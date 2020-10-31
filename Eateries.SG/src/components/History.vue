f8e3020cb3f467a5e1a17e9e46a464521368bc0f
Vinodjayakumar124 <60294470+Vinodjayakumar124@users.noreply.github.com>
2020-11-01 00:47:38 +0800

    History.vue page new
    Create a new History page to display the customer data

---
+++ Eateries.SG/src/components/History.vue
@@ -0,0 +1,44 @@
+<template>
+<body>
+    <div id="Banner" class="banner">
+        <div class="banner-wrap">
+            <div class="notice-text">Check out our promotions and don't forget to invite yourfriends for more perks!</div>
+        </div>
+    </div>
+    <div data-collapse="medium" data-animation="default" data-duration="400" id="Navigation">
+        <div class="navigation-container">
+            <div class="navigation-left"><img src="../assets/EateriesSG.png" loading="lazy" width="83" height="auto">
+                <div class="logo-text">EATERIES.SG</div>
+            </div>
+            <div class="navigation-right">
+                <router-link to="/" exact>Home</router-link>
+                <router-link class = "current"  to="/profile" exact>Profile</router-link>
+                <router-link to="/partnereateries" exact>Partner Eateries</router-link>
+                <router-link to="/currentbookings" exact>Current Bookings</router-link>
+                <router-link to="/declaration" exact>Declaration</router-link>
+            </div>
+        </div>
+    </div>
+    <div id="Header" class="header">
+        <div id="container-flex">
+            <div class="head-content">
+                <h1>History</h1>
+                <p>Testing! History page displayed here</p>
+            </div>
+        </div>
+    </div>
+
+</body>  
+</template>
+
+<script>
+    export default {
+        name: 'History',
+    }
+
+</script>
+
+<style>
+@import '../assets/basic_style.css';
+
+</style>
